b1b1414dcea7672cf05ad384c858995d45ce2405
Ritikchandra <114809062+Ritikchandra@users.noreply.github.com>
2023-04-26 01:57:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function getCountryByState(statename) {
   for (let i = 0; i<jsondata.length; i++) {
     const state = jsondata[i].states.find(s => s.name.toLowerCase().includes(statename.toLowerCase()));
     if (state) {
-      return jsondata[i].name;
+      return jsondata[i].name.toLowerCase();
     }
   }
   return null;
@@ -74,6 +74,10 @@ const jobfunction= ()=>{
   location.href=("#third-div")
   nav_header.classList.toggle("active");
 }
+
+const jobfunction1=()=>{
+  location.href=('#third-div');
+}
 const partnerfunction= ()=>{
   location.href=("#our-partners")
   nav_header.classList.toggle("active");
@@ -83,6 +87,11 @@ const joinfunction= ()=>{
   nav_header.classList.toggle("active");
 }
 // const textValue = document.getElementById("google-div").innerHTML;
+
+// var button1= getElementById('search-btn1')
+// button1.addEventListener("click", jobfunction1());
+
+
 
 search1.addEventListener("keypress", function(event) {
 
@@ -103,7 +112,7 @@ search1.addEventListener("keypress", function(event) {
 var array = [
   {
     name: 'google',
-    select: 'website developer',
+    select: 'website developer, all',
     country: 'india',
     title: 'frontend development',
     skills: 'html,css, javascript, react js',
@@ -112,7 +121,7 @@ var array = [
   },
   {
     name: 'twitter',
-    select: 'website developer',
+    select: 'website developer, all',
     country: 'usa , united states of america' ,
     title: 'backend development',
     skills: 'dsa, php, sql, java',
@@ -121,7 +130,7 @@ var array = [
   },
   {
     name: 'facebook',
-    select: 'application developer',
+    select: 'application developer, all',
     country: 'india',
     title: 'application development',
     skills: 'c++, backend ,computing, iot',
@@ -133,14 +142,14 @@ var array = [
     
     country: 'india',
     title: 'frontend development',
-    select: 'website developer',
+    select: 'website developer, all',
     skills: 'html,css, javascript, react js',
     image: "images/logo-apple.svg",
     type:'fulltime, full-time'
   },
   {
     name: 'microsoft',
-    select: 'designer',
+    select: 'designer, all',
     country: 'australia',
     title: 'ui/ux designer',
     skills: 'prototyping,development, information architecture',
@@ -149,7 +158,7 @@ var array = [
   },
   {
     name: 'amazon',
-    select: 'website developer',
+    select: 'website developer, all',
     country: 'india',
     title: 'backend development',
     skills: 'dsa , php , sql , java',
@@ -191,158 +200,85 @@ const generatejobs = (jobData) =>{
 
 let jobData = [...array];
 givejobs(jobData);
-var selectedValue;
 var newArray;
+
 function getSelectedValue(){
-  selectedValue = document.getElementById('select').value;
+
+  let searchInput1 = document.getElementById("search1");
+  let searchInput2 = document.getElementById("search2");
+  searchInput1.value = null;
+  searchInput2.value = null;
+
+//   var value1, value2;
+//   var countrynamefromstate1, countrynamefromstate2;
+//   searchInput1.addEventListener("input", e =>{
+//     value1 = e.target.value.toLowerCase()
+//     console.log(value1);
+//     countrynamefromstate1 = getCountryByState(value1);
+//   })
+//   searchInput2.addEventListener("input", e =>{
+//     value2 = e.target.value.toLowerCase()
+//     console.log(value2);
+//     countrynamefromstate2 = getCountryByState(value2);
+//   })
+ var selectedValue = document.getElementById('select').value.toLowerCase();
   console.log(selectedValue.toLowerCase());
-  
-  newArray = array.filter((array)=>
+//   if (value1 != ""||value2!=""){
+//   newArray = array.filter((array)=>
+//     {
+//       return ((array.name.includes(value1)
+//       || array.country.includes(countrynamefromstate1)||array.skills.includes(value1)||array.title.includes(value1)||array.type.includes(value1)||array.country.includes(value1)||array.name.includes(value2)
+//       || array.country.includes(countrynamefromstate2)||array.skills.includes(value2)||array.title.includes(value2)||array.type.includes(value2)||array.country.includes(value2))&&array.select.includes(selectedValue.toLowerCase()))
+//     })
+//     givejobs(newArray);}
+//     else
+{
+      newArray = array.filter((array)=>
     {
-      return (array.select.includes(selectedValue.toLowerCase()))
+      return (array.select.includes(selectedValue))
     })
     givejobs(newArray);
   }
+}
 
 
-const searchInput1 = document.getElementById("search1");
+let searchInput1 = document.getElementById("search1");
 // const value = searchInput1.value;
 // const countryName1 = getCountryByState(searchInput1.value);
-const searchInput2 = document.getElementById("search-space");
+let searchInput2 = document.getElementById("search2");
 // const countryName2 = getCountryByState(searchInput2.value);
 searchInput1.addEventListener("input", e =>{
-  const value = e.target.value 
+  const value = e.target.value.toLowerCase() 
+  // searchInput2 = "";
   console.log(value);
-  const countrynamefromstate = getCountryByState(value).toLowerCase();
+  const countrynamefromstate = getCountryByState(value);
   newArray = array.filter((array)=>
   {
-    return (array.name.includes(value)
-    || array.country.includes(countrynamefromstate)||array.skills.includes(value)||array.title.includes(value)||array.type.includes(value)||array.country.includes(value))
+    var selectedValue = document.getElementById('select').value;
+    return (((array.name.includes(value)
+    || array.country.includes(countrynamefromstate)||array.skills.includes(value)||array.title.includes(value)||array.type.includes(value)||array.country.includes(value))&&array.select.includes(selectedValue.toLowerCase())))
   })
   givejobs(newArray);
   
 } )
 searchInput2.addEventListener("input", e =>{
-  const value = e.target.value 
+  const value = e.target.value.toLowerCase() 
   console.log(value);
-  const countrynamefromstate = getCountryByState(value).toLowerCase();
-  const newArray = array.filter((array)=>
+  // var noSearch = document.querySelector('.no-search').classList;
+  // console.log(noSearch);
+  const countrynamefromstate = getCountryByState(value);
+  newArray = array.filter((array)=>
   {
-    return (array.name.includes(value)
-    || array.country.includes(countrynamefromstate)||array.skills.includes(value)||array.title.includes(value)||array.type.includes(value)||array.country.includes(value))
+    var selectedValue = document.getElementById('select').value;
+    return ((array.name.includes(value)
+    || array.country.includes(countrynamefromstate)||array.skills.includes(value)||array.title.includes(value)||array.type.includes(value)||array.country.includes(value))&& array.select.includes(selectedValue.toLowerCase()))
   })
+
   givejobs(newArray);
 } )
 
-var button1= getElementById('search-btn1')
-button1.addEventListener("click", jobfunction());
 
 
-
-
-
-// const jobfunction= ()=>{
-  //   location.href=("#third-div")
-  //   nav_header.classList.toggle("active");
-  // }
-  
-  
-  // const filterHandler = (event) => {
-    //   const searchText = event.target.value.toLowerCase();
-    
-    //   const filteredJobs = jobData.filter((jobData) => {
-      //     const countryName = getCountryByState(searchText);
-      //     return (
-        
-        //       jobData.name.toLowerCase().includes(searchText) ||
-        //       jobData.title.toLowerCase().includes(searchText) ||
-//       jobData.country.toLowerCase().includes(searchText) ||
-//       jobData.type.toLowerCase().includes(searchText) ||
-//       (countryName && jobData.country.toLowerCase().includes(countryName.toLowerCase())) ||
-//       jobData.skills.toLowerCase().includes(searchText)
-//       );
-//     });
-    
-
-// };
-
-// searchInput1.addEventListener('keyup', filterHandler);
-
-// const searchInputEl2 = document.getElementById("search-space");
-// const filterHandler2 = (event) => {
-//   const searchText = event.target.value.toLowerCase();
-  
-//   const filteredJobs = jobData.filter((jobData) => {
-//     const countryName = getCountryByState(searchText);
-//     return (
-//       jobData.title.toLowerCase().includes(searchText) ||
-//       jobData.name.toLowerCase().includes(searchText) ||
-//       // jobData.category.toLowerCase().includes(searchText) ||
-//       jobData.country.toLowerCase().includes(searchText) ||
-//       jobData.type.toLowerCase().includes(searchText) ||
-//       (countryName && jobData.country.toLowerCase().includes(countryName.toLowerCase())) ||
-//       jobData.skills.toLowerCase().includes(searchText)
-//       );
-//     });
-    
-//   givejobs(filteredJobs);};
-//     searchInput2.addEventListener('keyup', filterHandler2);
-  
-//     givejobs(jobData);
-    
-
-
-// const googleHide = document.getElementById('google-div');
-// if (array[0].name.includes(filter1)||array[0].country.includes(countryName1)||array[0].title.includes(filter1)||array[0].skills.includes(filter1)||array[0].type.includes(filter1)===false)
-
-
-
-// document.getElementById("search1").addEventListener("keyup", function(){
-//   var search = this.toLowerCase();
-//   // for ( i =0; i<array.length; i++){
-//     if (array[0].name.includes(search)||array[0].city.includes(search)||array[0].country.includes(search)||array[0].title.includes(search)||array[0].skills.includes(search)||array[0].type.includes(search)){
-//       document.getElementById('google-div').style.display.none;
-//     }
-  
-// })
-
-// const searchFun = () =>{
-//   let filter1 = document.getElementById('search1').value.toLowerCase();
-//   const countryName = getCountryByState(filter1.toLowerCase());
-//   let googleSearch= document.getElementById('google-div');
-//   let twitterSearch = document.getElementById('twitter-div');
-//   let facebookSearch = document.getElementById('facebook-div');
-//   let appleSearch = document.getElementById('apple-div');
-//   let microsoftSearch = document.getElementById('microsoft-div');
-//   let amazonSearch = document.getElementById('amazon-div');
-//   if (array[0].name.includes(filter1)||array[0].country.includes(countryName)||array[0].title.includes(filter1)||array[0].skills.includes(filter1)||array[0].type.includes(filter1)===false){
-//   googleSearch.style.display='none';}
-//   if (array[1].name.includes(filter1)||array[1].country.includes(countryName)||array[1].title.includes(filter1)||array[1].skills.includes(filter1)||array[1].type.includes(filter1)===false){
-//   twitterSearch.style.display='none';}
-//   if (array[2].name.includes(filter1)||array[2].country.includes(countryName)||array[2].title.includes(filter1)||array[2].skills.includes(filter1)||array[2].type.includes(filter1)===false){
-//   facebookSearch.style.display='none';}
-//   if (array[3].name.includes(filter1)||array[3].country.includes(countryName)||array[3].title.includes(filter1)||array[3].skills.includes(filter1)||array[3].type.includes(filter1)===false){
-//   appleSearch.style.display='none';}
-//   if (array[4].name.includes(filter1)||array[4].country.includes(countryName)||array[4].title.includes(filter1)||array[4].skills.includes(filter1)||array[4].type.includes(filter1)===false){
-//   microsoftSearch.style.display='none';}
-//   if (array[5].name.includes(filter1)||array[5].country.includes(countryName)||array[5].title.includes(filter1)||array[5].skills.includes(filter1)||array[5].type.includes(filter1)===false){
-//   amazonSearch.style.display='none';}
-  // window.location.reload();
-//   let filter2 = document.getElementById('search-space').value.toLowerCase();
-//   const countryName2 = getCountryByState(filter2).toLowerCase();
-//   if (array[0].name.includes(filter2)||array[0].city.includes(filter2)||array[0].country.includes(countryName2)||array[0].title.includes(filter2)||array[0].skills.includes(filter2)||array[0].type.includes(filter2)===false){
-//     googleSearch.style.display='none';}
-//     if (array[1].name.includes(filter2)||array[1].country.includes(countryName2)||array[1].title.includes(filter2)||array[1].skills.includes(filter2)||array[1].type.includes(filter2)===false){
-//     twitterSearch.style.display='none';}
-//     if (array[2].name.includes(filter2)||array[2].country.includes(countryName2)||array[2].title.includes(filter2)||array[2].skills.includes(filter2)||array[2].type.includes(filter2)===false){
-//     facebookSearch.style.display='none';}
-//     if (array[3].name.includes(filter2)||array[3].country.includes(countryName2)||array[3].title.includes(filter2)||array[3].skills.includes(filter2)||array[3].type.includes(filter2)===false){
-//     appleSearch.style.display='none';}
-//     if (array[4].name.includes(filter2)||array[4].country.includes(countryName2)||array[4].title.includes(filter2)||array[4].skills.includes(filter2)||array[4].type.includes(filter2)===false){
-//     microsoftSearch.style.display='none';}
-//     if (array[5].name.includes(filter2)||array[5].country.includes(countryName2)||array[5].title.includes(filter2)||array[5].skills.includes(filter2)||array[5].type.includes(filter2)===false){
-//     amazonSearch.style.display='none';}
-// }
 
 
 
